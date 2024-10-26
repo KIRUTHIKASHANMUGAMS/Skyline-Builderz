cdef4add3238a279fd7106601a7c956b1d1f1594
KIRUTHIKASHANMUGAMS <kiruthika@squashapps.com>
2024-10-26 11:53:12 +0530

    design changes

--- src/components/404/404Page/404Page.js
+++ src/components/404/404Page/404Page.js
@@ -10,7 +10,7 @@ function PageNotFound() {
         <img src={page} alt='' width="100%" />
         <div className='page-main'>
           <p className='pageNotfound'>Page Not Found</p>
-          <p className="service-right ">Dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi.Lorem ipsum.</p>
+          <p className="service-right-page ">Dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi.Lorem ipsum.</p>
        <button className='contact-btn-not-found grow_spin'><Link to="/"> Go To Home</Link> </button>
         </div>
        </Container>
